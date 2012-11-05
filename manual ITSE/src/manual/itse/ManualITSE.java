8aa5ad26cf4e09c1226e15ed1432f41c9fde68da
unknown <you@example.com>
2012-11-05 16:09:23 -0600

    realizacion de menu

--- manual ITSE/src/manual/itse/ManualITSE.java
+++ manual ITSE/src/manual/itse/ManualITSE.java
@@ -4,6 +4,8 @@
  */
 package manual.itse;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tania
@@ -16,6 +18,14 @@ public class ManualITSE
      */
     public static void main(String[] args)
     {
+         System.out.print("\nInstituto Tecnologico Superior de Escarcega \n veronca adriana hernandez almaro ");
+                System.out.print("\nAyuda");
+                System.out.print("\nLa opcion 1: Invierte una palabra  ");
+                System.out.print("\nLa opcion 2: Imprime el largo de la cadena ");
+                System.out.print("\nLa opcion 3: Lee una cadena y 2 numeros");
+                System.out.print("\nLa opcion 4: Lee una cadena y el rango entre 2 numeros");
+     
+                Scanner manual = new Scanner (System.in);
         // TODO code application logic here
     }
 }
